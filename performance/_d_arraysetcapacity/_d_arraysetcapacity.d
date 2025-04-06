1246eaf7a2ecf4efa51ded1e6af4dcb7e80f53e0
Albert24GG <guiman.albert09@gmail.com>
2025-04-06 19:24:39 +0300

    benchmark: Add missing `_d_arraysetcapacity` benchmark

---
+++ performance/_d_arraysetcapacity/_d_arraysetcapacity.d
@@ -0,0 +1,8 @@
+module performance._d_arraysetcapacity._d_arraysetcapacity;
+
+template GenTest(string Struct, string Size, string arr)
+{
+    const char[] GenTest = "void test" ~ Struct ~ '_' ~ Size ~ "Elems() { " ~ q{
+        } ~ Struct ~ "[] a;" ~ q{
+        } ~ "a.reserve(" ~ Size ~ "); }";
+}
