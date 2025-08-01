136b848abe5a43d99ecbba5d884bb87b348e4520
Albert24GG <guiman.albert09@gmail.com>
2025-08-01 17:11:55 +0300

    Add support for ldc + refactor

--- performance/run_benchmark.sh
+++ performance/run_benchmark.sh
@@ -8,46 +8,55 @@ if [ $# -ne 3 ] && [ $# -ne 2 ]; then
 	exit 1
 fi
 
-declare -A HOOKS_TEMPLATE_COMMITS
-HOOKS_TEMPLATE_COMMITS["_d_arraysetcapacity"]="master"
-HOOKS_TEMPLATE_COMMITS["_d_dynamic_cast"]="47c7321477ae8d97f881efa6a3de360da4f4a6d3"
-HOOKS_TEMPLATE_COMMITS["_d_paint_cast"]="9e7f0af277b7762efa6ae1a3a35a0b6cb7257d17"
-HOOKS_TEMPLATE_COMMITS["_d_class_cast"]="e0f222194cd412f71b2b4eabfef966f90849ce36"
-HOOKS_TEMPLATE_COMMITS["_d_interface_cast"]="6e8eb081cfa3cb58f1b308cb290ca8f364666cfd"
-HOOKS_TEMPLATE_COMMITS["_adEq2_equals"]="ad35200fe3bb6b7f6a9e08bd2d83bc4857cd441e"
-HOOKS_TEMPLATE_COMMITS["_adEq2_memcmp"]="ad35200fe3bb6b7f6a9e08bd2d83bc4857cd441e"
-HOOKS_TEMPLATE_COMMITS["__equals_memcmp"]="ad35200fe3bb6b7f6a9e08bd2d83bc4857cd441e"
-HOOKS_TEMPLATE_COMMITS["_d_arrayliteralTX"]="c2c8189599b894771393100ceae1ca2da30202d0"
+HOOK=$1
+RESULTS_FILE=$2
+if [[ "$2" = /* ]]; then
+	RESULTS_FILE=$2
+else
+	RESULTS_FILE="$PWD/$2"
+fi
+CRT_PATH=$PWD
 
-declare -A HOOKS_NON_TEMPLATE_COMMITS
-HOOKS_NON_TEMPLATE_COMMITS["_d_arraysetcapacity"]="master"
-HOOKS_NON_TEMPLATE_COMMITS["_d_dynamic_cast"]="99a390f3c6bae3b30c469222a9846273d300c407"
-HOOKS_NON_TEMPLATE_COMMITS["_d_paint_cast"]="8762d1eaee42119269b82a1b1b7063c89c1e6a69"
-HOOKS_NON_TEMPLATE_COMMITS["_d_class_cast"]="d234a544f13ee7293fc8108a0aba29685ae1ac38"
-HOOKS_NON_TEMPLATE_COMMITS["_d_interface_cast"]="9f573c494acc38855027462bde162fabea9cf33f"
-HOOKS_NON_TEMPLATE_COMMITS["_adEq2_equals"]="e0cf19144f2afed531cc2f40eee7e051994d4e98"
-HOOKS_NON_TEMPLATE_COMMITS["_adEq2_memcmp"]="e0cf19144f2afed531cc2f40eee7e051994d4e98"
-HOOKS_NON_TEMPLATE_COMMITS["__equals_memcmp"]="e0cf19144f2afed531cc2f40eee7e051994d4e98"
-HOOKS_NON_TEMPLATE_COMMITS["_d_arrayliteralTX"]="9749967b599870b2fdeb2744a3003e5cddf16242"
+BASE_DIR=$(realpath ~/src/dlang)
+D_COMPILER=${D_COMPILER:-dmd}
+DC_PATH="$BASE_DIR/$D_COMPILER"
+
+case $D_COMPILER in
+dmd)
+	DC=$DC_PATH/generated/linux/release/64/dmd
+	DC_FLAGS="-release -O -boundscheck=off -version=$HOOK -I$HOOK/"
+	PHOBOS_PATH=$BASE_DIR/phobos
+	declare -n TEMPLATED_COMMIT=DMD_TEMPLATED_COMMIT
+	declare -n NON_TEMPLATED_COMMIT=DMD_NON_TEMPLATED_COMMIT
+	;;
+gdc)
+	echo "GDC is not supported"
+	exit 1
+	;;
+ldc)
+	DC=$DC_PATH/build/bin/ldc2
+	DC_FLAGS="--release -O3 --boundscheck=off --d-version=$HOOK -I$HOOK/"
+	PHOBOS_PATH=$DC_PATH/runtime/phobos
+	declare -n TEMPLATED_COMMIT=LDC_TEMPLATED_COMMIT
+	declare -n NON_TEMPLATED_COMMIT=LDC_NON_TEMPLATED_COMMIT
+	;;
+*)
+	echo "Unknown D compiler: $D_COMPILER"
+	exit 1
+	;;
+esac
+
+source ./hook_commits.sh
 
 declare -A druntime_a_size
 declare -A phobos2_a_size
 declare -A phobos2_so_size
 
-HOOK=$1
-FILE=$2
 if [[ $# -eq 3 ]]; then
 	HOOK_BRANCH=$3
-elif [[ -v HOOKS_TEMPLATE_COMMITS[$HOOK] ]]; then
-	HOOK_BRANCH=${HOOKS_TEMPLATE_COMMITS[$HOOK]}
+elif [[ -v TEMPLATED_COMMIT[$HOOK] ]]; then
+	HOOK_BRANCH=${TEMPLATED_COMMIT[$HOOK]}
 fi
-
-DMD_PATH=~/dlang/dmd
-PHOBOS_PATH=$DMD_PATH/../phobos
-CRT_PATH=$(pwd)
-
-DMD_DIR=$(find $DMD_PATH/.. -maxdepth 1 -type d -name "dmd-2.*")
-source $DMD_DIR/activate
 
 function set_if_unset() {
 	local -n map_name=$1
@@ -61,60 +70,94 @@ function set_if_unset() {
 
 function get_current_branch() {
 	git_path=$1
-	cd $git_path
+	pushd $git_path >/dev/null
 	cur_branch=$(git rev-parse --abbrev-ref HEAD)
+	popd >/dev/null
 	echo $cur_branch
 }
 
 function restore_branch() {
 	git_path=$1
 	cur_branch=$2
-	cd $git_path
-	git checkout $cur_branch
+	pushd $git_path >/dev/null
+	git checkout -f $cur_branch
+	git submodule update --init
+	popd >/dev/null
 }
 
 function sync_repos() {
 	commit_sha=$1
 	skip_phobos=${2:-false}
 
-	cd $DMD_PATH
-	commit_date=$(git show -s --format=%ci $commit_sha)
+	pushd $DC_PATH >/dev/null
 
-	git checkout $commit_sha
-	make clean &>/dev/null
-	make -j$(nproc) &>/dev/null
+	case $D_COMPILER in
+	dmd)
+		commit_date=$(git show -s --format=%ci $commit_sha)
+		git checkout $commit_sha
+		make clean &>/dev/null
 
-	druntime_a_bytes=$(stat -Lc %s generated/linux/release/64/libdruntime.a)
-	set_if_unset druntime_a_size "$commit_sha" "$druntime_a_bytes"
+		if [[ $skip_phobos == false ]]; then
+			pushd $PHOBOS_PATH >/dev/null
+			git checkout $(git rev-list -n 1 --before="$commit_date" master)
+			make clean &>/dev/null
+			make -j$(nproc) &>/dev/null
+			popd >/dev/null
 
-	cd $PHOBOS_PATH
-	git checkout $(git rev-list -n 1 --before="$commit_date" master)
-	make clean &>/dev/null
+			druntime_a_path=$PHOBOS_PATH/generated/linux/release/64/libdruntime.a
+			libphobos2_a_path=$PHOBOS_PATH/generated/linux/release/64/libphobos2.a
+			libphobos2_so_path=$PHOBOS_PATH/generated/linux/release/64/libphobos2.so
+		fi
+		make -j$(nproc) &>/dev/null
 
-	if [[ $skip_phobos == true ]]; then
-		return
+		;;
+	gdc)
+		git checkout $commit_sha
+		;;
+	ldc)
+		rm -r runtime
+		git reset --hard HEAD
+		git checkout -f $commit_sha
+		git submodule update --init &>/dev/null
+
+		rm -r build &>/dev/null
+		mkdir build
+		cmake -S . -B build &>/dev/null
+
+		cd build
+		make clean &>/dev/null
+		make -j$(nproc) ldc2
+		if [[ $skip_phobos == false ]]; then
+			make -C runtime -j$(nproc) druntime-ldc phobos2-ldc phobos2-ldc-shared &>/dev/null
+			druntime_a_path=$(realpath lib/libdruntime-ldc.a)
+			libphobos2_a_path=$(realpath lib/libphobos2-ldc.a)
+			libphobos2_so_path=$(realpath lib/libphobos2-ldc-shared.so)
+		fi
+
+		;;
+	esac
+
+	if [[ $skip_phobos == false ]]; then
+		druntime_a_bytes=$(stat -Lc %s "$druntime_a_path")
+		set_if_unset druntime_a_size "$commit_sha" "$druntime_a_bytes"
+
+		libphobos2_a_bytes=$(stat -Lc %s "$libphobos2_a_path")
+		set_if_unset phobos2_a_size "$commit_sha" "$libphobos2_a_bytes"
+
+		libphobos2_so_bytes=$(stat -Lc %s "$libphobos2_so_path")
+		set_if_unset phobos2_so_size "$commit_sha" "$libphobos2_so_bytes"
 	fi
 
-	make -j$(nproc) &>/dev/null
-
-	libphobos2_a_bytes=$(stat -Lc %s generated/linux/release/64/libphobos2.a)
-	set_if_unset phobos2_a_size "$commit_sha" "$libphobos2_a_bytes"
-
-	libphobos2_so_bytes=$(stat -Lc %s generated/linux/release/64/libphobos2.so)
-	set_if_unset phobos2_so_size "$commit_sha" "$libphobos2_so_bytes"
+	popd >/dev/null
 }
 
 function test_commit() {
 	sync_repos $1
 
-	cd $CRT_PATH
-	CFLAGS="-release -O -boundscheck=off -version=$HOOK -I$HOOK/"
-	CC=$DMD_PATH/generated/linux/release/64/dmd
-
-	# TODO: clean up
-	# CFLAGS="$CFLAGS" CC=$CC make;
-	$CC $CFLAGS array_benchmark.d
-	./array_benchmark >>$FILE
+	pushd $CRT_PATH >/dev/null
+	$DC $DC_FLAGS array_benchmark.d
+	./array_benchmark >>$RESULTS_FILE
+	popd >/dev/null
 }
 
 function compilation_bench() {
@@ -122,16 +165,27 @@ function compilation_bench() {
 
 	sync_repos $commit_sha true
 
-	cd $PHOBOS_PATH
+	case $D_COMPILER in
+	dmd)
+		pushd $PHOBOS_PATH >/dev/null
+		compile_bench_command="make clean && make $DC_PATH/druntime clean && make -j$(nproc)"
+		;;
+	gdc) ;;
+	ldc)
+		pushd $DC_PATH/build/runtime >/dev/null
+		compile_bench_command="make clean && make -j$(nproc) druntime-ldc phobos2-ldc"
+		;;
+	esac
 
-	perf_out=$(perf stat -r 100 bash -c "(make clean && make $DMD_PATH/druntime clean && make -j$(nproc)) &>/dev/null" 2>&1 | tail -n2 | head -n1)
+	perf_out=$(perf stat -r 100 bash -c "($compile_bench_command) &>/dev/null" 2>&1 | tail -n2 | head -n1)
+
+	popd >/dev/null
 
 	avg_time=$(echo $perf_out | awk '{print $1}')
 	stddev=$(echo $perf_out | awk '{print $3}')
 	stddev_percent=$(echo $perf_out | awk '{print $(NF-1)}')
 
-	cd $CRT_PATH
-	echo "Average time: $avg_time seconds, Standard deviation: $stddev (+- $stddev_percent) seconds" >>$FILE
+	echo "Average time: $avg_time seconds, Standard deviation: $stddev (+- $stddev_percent) seconds" >>$RESULTS_FILE
 }
 
 function compute_percentage_change() {
@@ -151,25 +205,27 @@ function test_hook() {
 	baseline_commit=$1
 	hook_commit=$2
 
-	cur_dmd_branch=$(get_current_branch $DMD_PATH)
-	cur_phobos_branch=$(get_current_branch $PHOBOS_PATH)
+	cur_dmd_branch=$(get_current_branch $DC_PATH)
+	if [[ $D_COMPILER == "dmd" ]]; then
+		cur_phobos_branch=$(get_current_branch $PHOBOS_PATH)
+	fi
 
 	for i in {1..5}; do
-		echo -e "\n============================================================" >>$FILE
-		echo "Testing non-template hook - Commit: ${baseline_commit}" >>$FILE
+		echo -e "\n============================================================" >>$RESULTS_FILE
+		echo "Testing non-template hook - Commit: ${baseline_commit}" >>$RESULTS_FILE
 		test_commit $baseline_commit
 
-		echo -e "\n============================================================" >>$FILE
-		echo "Testing template hook - Commit: ${hook_commit}" >>$FILE
+		echo -e "\n============================================================" >>$RESULTS_FILE
+		echo "Testing template hook - Commit: ${hook_commit}" >>$RESULTS_FILE
 		test_commit $hook_commit
 	done
 
-	echo -e "\n============================================================" >>$FILE
-	echo "Compilation performance non-template hook - Commit: ${baseline_commit}" >>$FILE
+	echo -e "\n============================================================" >>$RESULTS_FILE
+	echo "Compilation performance non-template hook - Commit: ${baseline_commit}" >>$RESULTS_FILE
 	compilation_bench $baseline_commit
 
-	echo -e "\n============================================================" >>$FILE
-	echo "Compilation performance template hook - Commit: ${hook_commit}" >>$FILE
+	echo -e "\n============================================================" >>$RESULTS_FILE
+	echo "Compilation performance template hook - Commit: ${hook_commit}" >>$RESULTS_FILE
 	compilation_bench $hook_commit
 
 	druntime_a_size_old=${druntime_a_size[$baseline_commit]}
@@ -181,25 +237,37 @@ function test_hook() {
 	phobos2_so_size_old=${phobos2_so_size[$baseline_commit]}
 	phobos2_so_size_new=${phobos2_so_size[$hook_commit]}
 
-	echo -e "\n============================================================" >>$FILE
-	echo "libdruntime.a size: old=${druntime_a_size_old} B / new=${druntime_a_size_new} B => $(compute_percentage_change $druntime_a_size_old $druntime_a_size_new) change" >>$FILE
-	echo "libphobos2.a size: old=${phobos2_a_size_old} B / new=${phobos2_a_size_new} B => $(compute_percentage_change $phobos2_a_size_old $phobos2_a_size_new) change" >>$FILE
-	echo "libphobos2.so size: old=${phobos2_so_size_old} B / new=${phobos2_so_size_new} B => $(compute_percentage_change $phobos2_so_size_old $phobos2_so_size_new) change" >>$FILE
+	echo -e "\n============================================================" >>$RESULTS_FILE
+	echo "libdruntime.a size: old=${druntime_a_size_old} B / new=${druntime_a_size_new} B => $(compute_percentage_change $druntime_a_size_old $druntime_a_size_new) change" >>$RESULTS_FILE
+	echo "libphobos2.a size: old=${phobos2_a_size_old} B / new=${phobos2_a_size_new} B => $(compute_percentage_change $phobos2_a_size_old $phobos2_a_size_new) change" >>$RESULTS_FILE
+	echo "libphobos2.so size: old=${phobos2_so_size_old} B / new=${phobos2_so_size_new} B => $(compute_percentage_change $phobos2_so_size_old $phobos2_so_size_new) change" >>$RESULTS_FILE
 
-	restore_branch $DMD_PATH $cur_dmd_branch
-	restore_branch $PHOBOS_PATH $cur_phobos_branch
+	if [[ $D_COMPILER == "ldc" ]]; then
+		pushd $DC_PATH >/dev/null
+		rm -r runtime
+		git reset --hard HEAD
+	fi
+	restore_branch $DC_PATH $cur_dmd_branch
+	if [[ $D_COMPILER == "dmd" ]]; then
+		restore_branch $PHOBOS_PATH $cur_phobos_branch
+	fi
 }
 
 if [ $HOOK_BRANCH != "" ]; then
 	echo "branch"
-	base_branch=${HOOKS_NON_TEMPLATE_COMMITS[$HOOK]:-"master"}
+	base_branch=${NON_TEMPLATED_COMMIT[$HOOK]}
+	if [[ $base_branch == "" ]]; then
+		echo "Non-template commit hash for hook '$HOOK' not found."
+		exit 1
+	fi
 	test_hook $base_branch $HOOK_BRANCH
 else
-	echo "no branch"
-	cd $DMD_PATH
-	git checkout master
-	hook_commit=$(git log --grep="Translate $HOOK" | head -n 1 | cut -d ' ' -f 2)
-	test_hook "$hook_commit^" $hook_commit
+	echo "no branch -> unsupported"
+	exit 1
+	# cd $DMD_PATH
+	# git checkout master
+	# hook_commit=$(git log --grep="Translate $HOOK" | head -n 1 | cut -d ' ' -f 2)
+	# test_hook "$hook_commit^" $hook_commit
 fi
 
 cd $CRT_PATH
