cb58feb43a00f1569ba7d3626e091ebe91774a46
Albert24GG <guiman.albert09@gmail.com>
2025-08-05 10:11:12 +0300

    Fix `arrayassign` and `newarray` benchmarks

--- performance/_d_arrayassign/_d_arrayassign.d
+++ performance/_d_arrayassign/_d_arrayassign.d
@@ -2,5 +2,8 @@ module _d_arrayassign;
 
 template GenTest(string Struct, string Size, string arr)
 {
-    const char[] GenTest = "void test" ~ Struct ~ '_' ~ Size ~ "Elems() { " ~ Struct ~ "[] a = " ~ arr ~ "; }";
+    const char[] GenTest ="void test" ~ Struct ~ '_' ~ Size ~ "Elems() {"
+    ~ Struct ~ "[" ~ arr ~ ".length] a;"
+    ~ "a = " ~ arr ~ ";"
+    ~"}";
 }
