c0028300eaeae128d13d8c5239a4718bb630da27
Albert24GG <guiman.albert09@gmail.com>
2025-08-13 16:40:57 +0300

    Add `_d_newarrayiT` bench & update `_d_newarrayT`

--- performance/_d_newarrayiT/_d_newarrayiT.d
+++ performance/_d_newarrayiT/_d_newarrayiT.d
@@ -1,10 +1,10 @@
-module _d_newarrayT;
+module _d_newarrayiT;
 
 template GenTest(string Struct, string Size, string arr)
 {
     const char[] GenTest = "void* dummy" ~ Struct ~ '_' ~ Size ~ ";"
         ~ "void test" ~ Struct ~ '_' ~ Size ~ "Elems() { "
-        ~ "struct S{char[" ~ Size ~ "]a = 0;};"
+        ~ "struct S{char[" ~ Size ~ "]a = 1;};"
         ~ "S[] a = new S[" ~ Size ~ "];"
         ~ "dummy" ~ Struct ~ '_' ~ Size ~ " = a.ptr;"
         ~ "}";
